dfc1e681232760ed1f1925904ff82a52e0edb0d5
Alex <tseplyaeff.alex@yandex.ru>
2020-09-21 21:40:46 +0900

    ver 0

---
+++ DiffusionModel/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace DiffusionModel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button_Stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture
+            // 
+            this.picture.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(171)))), ((int)(((byte)(211)))));
+            this.picture.Location = new System.Drawing.Point(71, 35);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(414, 332);
+            this.picture.TabIndex = 0;
+            this.picture.TabStop = false;
+            // 
+            // button_Start
+            // 
+            this.button_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Start.Location = new System.Drawing.Point(188, 373);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(205, 43);
+            this.button_Start.TabIndex = 1;
+            this.button_Start.Text = "Запустить процесс диффузии";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button_Stop
+            // 
+            this.button_Stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Stop.Location = new System.Drawing.Point(188, 373);
+            this.button_Stop.Name = "button_Stop";
+            this.button_Stop.Size = new System.Drawing.Size(205, 43);
+            this.button_Stop.TabIndex = 2;
+            this.button_Stop.Text = "Остановить процесс диффузии";
+            this.button_Stop.UseVisualStyleBackColor = true;
+            this.button_Stop.Click += new System.EventHandler(this.button_Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 428);
+            this.Controls.Add(this.button_Stop);
+            this.Controls.Add(this.button_Start);
+            this.Controls.Add(this.picture);
+            this.MaximumSize = new System.Drawing.Size(573, 467);
+            this.MinimumSize = new System.Drawing.Size(573, 467);
+            this.Name = "Form1";
+            this.Text = "Моделирование процесса диффузии";
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button_Stop;
+    }
+}
+
---
+++ DiffusionModel/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiffusionModel
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Graphics g;
+        Pen pen;
+        const int CountMolecules = 100;
+        const int CenterPictureX = 207;
+        const int HeightPicture = 332;
+        Random rnd = new Random();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            button_Stop.Visible = false;
+            pen = new Pen(Color.Black);
+            bmp = new Bitmap(picture.Width, picture.Height);
+            g = Graphics.FromImage(bmp);
+            pen.Width = 5;
+            DrawLine();
+            RandomDots();
+            InitializeTimers();
+        }
+
+        private void DrawLine()
+        {
+            g.DrawLine(pen, CenterPictureX, 0, CenterPictureX, HeightPicture);
+            picture.Image = bmp;
+        }
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+            {
+                timer1.Enabled = true;
+                timer1.Start();
+            }
+            button_Stop.Visible = true;
+        }
+
+        private void InitializeTimers()
+        {
+            // Таймер для быстрой смены кадров
+            timer1.Interval = 100;
+            timer1.Tick += new EventHandler(timer1_Tick);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            g.Clear(Color.FromArgb(122, 171, 211));
+            RandomDots();
+        }
+
+        private void RandomDots()
+        {
+            int DotX, DotY;
+            for (int i = 0; i < CountMolecules; i++)
+            {
+                int tmp;
+                bool flag = false;
+                do
+                {
+                    DotX = rnd.Next(0, (CenterPictureX * 2) - 6);
+                    DotY = rnd.Next(5, HeightPicture - 6);
+                    tmp = CenterPictureX - DotX;
+                    if ((tmp == CenterPictureX) || (tmp < CenterPictureX))
+                        flag = true;
+                } while (!flag);
+                g.DrawEllipse(pen, DotX, DotY, 5, 5);
+                picture.Image = bmp;
+            }
+        }
+
+        private void button_Stop_Click(object sender, EventArgs e)
+        {
+            button_Stop.Visible = false;
+            timer1.Enabled = false;
+            timer1.Stop();
+            DrawLine();
+        }
+    }
+}
